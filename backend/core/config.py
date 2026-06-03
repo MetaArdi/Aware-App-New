ad57a9f9d982bf33c831bd995bd58c86e9622c0c
MrAnxious <metaflash55@gmail.com>
2026-06-04 02:14:50 +0700

    Fix absolute path in config to allow linux deployment

--- backend/core/config.py
+++ backend/core/config.py
@@ -8,4 +8,4 @@ SECRET_KEY                  = os.getenv("SECRET_KEY", "fallback-secret-key-chang
 ALGORITHM                   = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 480))
 ML_SERVER_URL               = os.getenv("ML_SERVER_URL", "http://localhost:8000/ml")
-MODEL_PATH                  = os.getenv("MODEL_PATH", "d:/Dev/Capstone Coding Camp Workspace/resource/aware_fatigue_savedmodel-20260524T115612Z-3-001/aware_fatigue_savedmodel/aware_fatigue_model.tflite")
+MODEL_PATH                  = os.getenv("MODEL_PATH", "../model/exports/aware_fatigue_model.tflite")
